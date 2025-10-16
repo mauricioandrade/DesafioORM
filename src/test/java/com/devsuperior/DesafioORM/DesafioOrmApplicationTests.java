ba1bc5eb5bad1b2ba351cc39293c349fe2bbd725
mauricioandrade <mauricio_andrade@outlook.com>
2025-10-16 00:32:56 -0300

    first commit

---
+++ src/test/java/com/devsuperior/DesafioORM/DesafioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DesafioORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
